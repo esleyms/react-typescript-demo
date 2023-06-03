57689cf8ab45eddb49f26d15b2bc0ef88f168fb5
esleyms <101306305+esleyms@users.noreply.github.com>
2023-06-03 19:12:26 -0300

    ALT 101

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import { LoggedIn } from './Components/LoggedIn';
 
 
 
 function App() {
   return (
     <div className="App">
-    
+    <LoggedIn/>
     </div>
   );
 }
