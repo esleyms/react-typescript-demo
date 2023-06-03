175684ed8ecaca81e8803f2ba14cf872074b9511
esleyms <101306305+esleyms@users.noreply.github.com>
2023-06-03 19:45:48 -0300

    102 - 10 - useState Future Value

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { LoggedIn } from './Components/LoggedIn';
+import { User } from './Components/User';
 
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
     <LoggedIn/>
+    <User/>
     </div>
   );
 }
